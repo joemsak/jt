5ef7cc2a5c1026e9196c9c6e543f098cd5ee1df7
Joe Sak <joe@joesak.com>
2016-04-06 12:10:25 -0400

    Add profile list

--- src/components/app.js
+++ src/components/app.js
@@ -1,10 +1,11 @@
 import React from 'react';
 import { Component } from 'react';
+import ProfileList from './profile/list';
 
 export default class App extends Component {
   render() {
     return (
-      <div>React simple starter</div>
+      <ProfileList />
     );
   }
 }
---
+++ src/components/profile/list.js
@@ -0,0 +1,11 @@
+import React from 'react';
+import { Component } from 'react';
+import ReactFireMixin from 'reactfire';
+
+export default class ProfileList extends Component {
+  render() {
+    return (
+      <div>Profile List</div>
+    );
+  }
+}
